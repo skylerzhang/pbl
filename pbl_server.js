76f00d0d2fdf6952243543cee3bdc943c58192d7
skyler <zhangning333@gmail.com>
2014-06-20 10:06:29 +0800

    瀑布流
    BAE+NODEJS+MYSQL 搭建瀑布流

---
+++ pbl_server.js
@@ -0,0 +1,74 @@
+var http=require('http');
+var fs=require('fs');
+var qs=require('querystring');
+var mysql=require('mysql');
+
+var server=http.createServer(function (request, response){
+	var str='';
+	
+	request.addListener('data', function (data){
+		str+=data;
+	});
+	request.addListener('end', function (){
+		var post=qs.parse(str);
+		
+		if(request.url.indexOf('?')!=-1)
+		{
+			var arr=request.url.split('?');
+			
+			var url=arr[0];
+			var get=qs.parse(arr[1]);
+		}
+		else
+		{
+			var url=request.url;
+			var get={};
+		}
+		
+		if(url=='/pubuliu')
+		{
+			var n=parseInt(get.page);
+			
+			if(n<1 || isNaN(n))
+			{
+				n=1;
+			}
+			
+			var pageSize=10;
+			var s=(n-1)*pageSize;
+			
+			var sql="SELECT * FROM pubuliu LIMIT "+s+","+pageSize;
+			
+			var db=mysql.createConnection({host: 'sqld.duapp.com', user: 'Zsg9BMEHNCXZNSFjHu276bdd', password: '9N3t5ZBffW67q1xRANfIRdqjr8qhsVwr', database: 'gTdXkAwiqMPbkILLKXXg'});
+			
+			db.query(sql, function (err, data){
+				if(err)
+				{
+					response.write('{"err": 1, "msg": "数据库出错"}');
+					response.end();
+				}
+				else
+				{
+					response.write('{"err": 0, "data": '+JSON.stringify(data)+'}');
+					response.end();
+				}
+			});
+		}
+		else
+		{
+			fs.readFile('www'+url, function (err, data){
+				if(err)
+				{
+					response.write('404');
+				}
+				else
+				{
+					response.write(data);
+				}
+				response.end();
+			});
+		}
+	});
+});
+
+server.listen(18080);
